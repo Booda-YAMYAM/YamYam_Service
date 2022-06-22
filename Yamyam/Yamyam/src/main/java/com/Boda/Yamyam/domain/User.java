5d4f9371d92cc34c720d28795358043d4a8ec94a
leejihoon0312 <82079111+leejihoon0312@users.noreply.github.com>
2022-06-22 19:12:15 +0900

    테스트ㅇㅇㄹㅇㄹ

--- Yamyam/Yamyam/src/main/java/com/Boda/Yamyam/domain/User.java
+++ Yamyam/Yamyam/src/main/java/com/Boda/Yamyam/domain/User.java
@@ -10,6 +10,7 @@ import java.util.List;
 @Entity
 @Getter @Setter
 public class User {
+
     @Id
     private String userId;
 
---
+++ Yamyam/Yamyam/src/main/java/com/Boda/Yamyam/repository/RestaurantRepository.java
@@ -0,0 +1,22 @@
+package com.Boda.Yamyam.repository;
+
+import com.Boda.Yamyam.domain.Restaurant;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import java.util.List;
+
+@Repository
+@RequiredArgsConstructor
+public class RestaurantRepository {
+
+    private final EntityManager em;
+
+    public void save(Restaurant restaurant){em.persist(restaurant);}
+
+    public List<Restaurant> findAllRestaurant() {
+        return em.createQuery("select r from Restaurant r", Restaurant.class)
+                .getResultList();
+    }
+}
