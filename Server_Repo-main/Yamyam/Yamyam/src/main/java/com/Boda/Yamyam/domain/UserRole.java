e84d4f34a019f670e3e4b345bfe51534be0fdf7f
qkdk <86948395+qkdk@users.noreply.github.com>
2022-06-24 04:22:42 +0900

    feature : image-process

--- Server_Repo-main/Yamyam/Yamyam/src/main/java/com/Boda/Yamyam/domain/UserRole.java
+++
@@ -1,5 +0,0 @@
-package com.Boda.Yamyam.domain;
-
-public enum UserRole {
-    USER_ROLE, USER_ADMIN;
-}
--- Server_Repo-main/Yamyam/Yamyam/src/main/java/com/Boda/Yamyam/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.Boda.Yamyam.repository;
-
-import com.Boda.Yamyam.domain.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, String> {
-
-    User findByUserId(String userId);
-}
